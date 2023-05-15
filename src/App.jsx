50a1528ae606328ecb0a6911763f503cf445457a
EnowMyke-A <enowmyke944@gmail.com>
2023-05-15 20:10:55 +0200

    Submission

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './component/header'
+//import HeaderSocial from './component/HeaderSocial'
+//import Navbar from './components/navbar/Navbar'
+//import About from './components/about/About'
+//import Experience from './components/experience/Experience'
+//import Services from './components/services/Services'
+//import Portfolio from './components/portfolio/Portfolio'
+//import Testimonial from './components/testimonial/Testimonial'
+import Contact from './component/Contact'
+
+function App() {
+return (
+<>
+<Header/>
+<Contact/>
+</>
+);
+}
+export default App;
